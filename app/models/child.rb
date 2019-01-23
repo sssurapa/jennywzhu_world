c9c0fa5a045f2509dcf3f2b974f672120753fd58
Jenny Zhu <jwzhu@andrew.cmu.edu>
2019-01-23 02:54:35 +0000

    finish child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,6 +5,9 @@ class Child < ApplicationRecord
     
     validates_presence_of :first_name
     validates_presence_of :last_name
+
+    scope :alphabetical, -> { order(:last_name, :first_name) }
+    scope :active, -> { where(:active  => true) }
     
     def name 
         return "#{first_name} #{last_name}"
